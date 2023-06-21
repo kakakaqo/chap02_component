f73077bd675cf9d7c1559c2664d97b1d693be43f
kakakaqo <alsdn481@naver.com>
2023-06-21 16:28:15 +0900

    프롭, 조건부렌더링, 리스트, 폼

---
+++ src/component/Hello.js
@@ -0,0 +1,12 @@
+import World from "./World";
+import styles from "./Hello.module.css";
+
+export default function Hello() {
+    return(
+        <div className={styles.box}>
+            <h1>Hello </h1>
+            <World />
+        </div>
+    )
+    
+}   
